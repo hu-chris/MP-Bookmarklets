21921b6c21489efc2865bc34e805a0ef0cf7bfcb
hu-chris <birdbrainguy@gmail.com>
2017-12-12 00:26:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
 document.getElementById('Page_GetQuote__ProductQuestionCodes_QUOTE__0__QuestionData_PostalCodeAnswer').value += '99999';
-
-alert('Hello World');
+document.getElementById('Page_GetQuote__ProductQuestionCodes_QUOTE__2__QuestionData_UserAnswer').selectedIndex = 12;
