16f1c2ff3e38a8e34f4cadff3acd205e8d9e64f0
hu-chris <birdbrainguy@gmail.com>
2017-12-11 18:52:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('Page_GetQuote__ProductQuestionCodes_QUOTE__0__QuestionData_PostalCodeAnswer').innerHTML = '99999';
+document.getElementById('Page_GetQuote__ProductQuestionCodes_QUOTE__0__QuestionData_PostalCodeAnswer').value += '99999';
 
 alert('Hello World');
