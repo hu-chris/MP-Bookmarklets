7a39709474d74d2eeafdffd5d3f9c3bbe69da52b
hu-chris <birdbrainguy@gmail.com>
2017-12-11 18:30:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.getElementsByClassName('PostalCodeAnswer').innerHTML = '99999';
+var div = document.createElement("div");
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.background = "red";
+div.style.color = "white";
+div.innerHTML = "Hello";
+
+document.body.appendChild(div);
