3c310be3857f8f436b0e33906666ce233cca1897
hu-chris <birdbrainguy@gmail.com>
2017-12-13 00:45:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById('Page_GetQuote__ProductQuestionCodes_QUOTE__0__QuestionData_PostalCodeAnswer').value += '29222';
 document.getElementById('Page_GetQuote__ProductQuestionCodes_QUOTE__2__QuestionData_UserAnswer').selectedIndex = 12;
 document.getElementById('Page_GetQuote__ProductQuestionCodes_QUOTE__7__QuestionData_DateAnswer').value += '01';
+document.getElementById('Page_GetQuote__ProductQuestionCodes_QUOTE__7__QuestionData_DateAnswer').value += '01';
+document.getElementById('Page_GetQuote__ProductQuestionCodes_QUOTE__7__QuestionData_DateAnswer').value += '1965';
