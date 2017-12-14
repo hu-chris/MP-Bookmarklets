9c4cb822a10a4b4f534c2070c7c3544219c0126f
hu-chris <birdbrainguy@gmail.com>
2017-12-13 18:08:03 -0700

    Update GetRates.js

--- GetRates.js
+++ GetRates.js
@@ -1,3 +1,5 @@
 var a = document.getElementsByClassName('monthlyPremium');
 console.log(a[0]);
-console.log(a[0].value);
+var b = document.getElementById('bundleTotalToDisplayPremium_238291822335STS50100003months1000078');
+console.log(b);
+console.log(b.value);
