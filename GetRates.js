01067746c65d0ee44b3404db3642d5ad12c56091
hu-chris <birdbrainguy@gmail.com>
2017-12-13 18:29:27 -0700

    Update GetRates.js

--- GetRates.js
+++ GetRates.js
@@ -1,3 +1,9 @@
+var premList = [];
 var a = document.getElementsByClassName('monthlyPremium');
-console.log(a[0]);
-var b = document.getElementById('bundleTotalToDisplayPremium_238291822335STS50100003months1000078').value;
+for (i=0;i<a.length;i++) {
+  var b = a[i].innerText;
+  premList.push(b);
+}
+
+console.log(premList);
+
